876e281b7f44693dcc7ad241320dfb7ea178d867
Jcorrales07 <corralesjoe07@gmail.com>
2022-10-07 18:36:32 -0600

    Con esto entendi que si aprendi sobre el DOM y voy aprendiendo de las APIs

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const API = 'https://api.thecatapi.com/v1/images/search'
-const queryParameter = '?limit=3'
-const API_KEY = 'live_TYj5SIoubXBpSyW2Ax1ftQetdUJcN72RyJJswjHvWOVnPHQ06oj7h61L5SjWUUgI'
-const queryApiKeyParameter = `?api_key=${API_KEY}`
-const API_FAVORITES = `https://api.thecatapi.com/v1/favourites${queryApiKeyParameter}`
+const API_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=10';
+const API_KEY =
+    'live_TYj5SIoubXBpSyW2Ax1ftQetdUJcN72RyJJswjHvWOVnPHQ06oj7h61L5SjWUUgI';
+const queryApiKeyParameter = `?api_key=${API_KEY}`;
+const API_FAVORITES = `https://api.thecatapi.com/v1/favourites${queryApiKeyParameter}`;
 
 // Los ENDPOINTS son las rutas que tiene una api para dar contenido especifico
 // Ejemplo
@@ -12,7 +12,7 @@ const API_FAVORITES = `https://api.thecatapi.com/v1/favourites${queryApiKeyParam
 // api.com/images/michi34
 
 // Los QUERY PARAMETERS  sirven para hacer un filtro de la especificacion de contenido
-// Que estamos pidiendo, estariamos haciendo un filtro del filtro 
+// Que estamos pidiendo, estariamos haciendo un filtro del filtro
 // Los query parameters se utilizan en los endpoints
 // HAY QUE REVISAR LA DOCUMENTACION DE LA API PARA VER QUE ENDPOINTS
 // SOPORTAN QUE QUERIES
@@ -24,48 +24,83 @@ const API_FAVORITES = `https://api.thecatapi.com/v1/favourites${queryApiKeyParam
 // Twitter acepta uno de text
 // ========= Para poder poner otro query parameter se usar el &
 
-const img1 = document.getElementById('img1')
-const img2 = document.getElementById('img2')
-const img3 = document.getElementById('img3')
-const button = document.getElementById('nextPhotoBtn')
-
+const divRandomMichis = document.getElementById('randomPhotoMichis');
+const button = document.getElementById('nextPhotoBtn');
+const errorSpan = document.getElementById('errorSpan')
 
 // Yo lo hice asi
 async function fetchData(apiUrl) {
-    const response = await fetch(apiUrl)
-    const data = await response.json()
-    console.log(data)
+    const response = await fetch(apiUrl);
+    const data = await response.json();
     return data;
 }
 
-const getResults = () => {
-    const results = fetchData(`${API}${queryParameter}&${queryApiKeyParameter}`)
-
-    results.then(data => {
-        console.log(data)
-        img1.src = data[0].url
-        img2.src = data[1].url
-        img3.src = data[2].url
-    })
-}
-
+const getResults = async () => {
+    const response = await fetch(`${API_RANDOM}&${queryApiKeyParameter}`)
+    const data = await response.json();
+    
+    if (response.status !== 200) {
+        errorSpan.innerText = `Error: ${response.status}`
+        return
+    }
+    
+    data.forEach((item) => {
+        let article = document.createElement('article');
+        article.innerHTML = `
+            <img alt="Foto gatito aleatorio" width="250px" src="${item.url}">
+            <button>Guardar en favoritos</button>
+        `;
+        divRandomMichis.append(article);
+        console.log(item.id)
+    });
+};
 
 async function getFavoriteMichis() {
-    const results = await fetchData(API_FAVORITES)
-    console.log('results', results)
+    const response = await fetch(API_FAVORITES)
+    const data = await response.json()
+
+    if (response.status !== 200) {
+        errorSpan.innerHTML = `Error: ${response.status}`
+    }
+
+    const divFavoriteMichis = document.getElementById('favoritesPhotoMichis')
+    divFavoriteMichis.style.display = "block"
+    console.log('dataaaa', data)
+
+    data.forEach(michi => {
+        if (michi.image.url) {
+            const article = document.createElement('article')
+            const img = document.createElement('img')
+            img.src = michi.image.url
+            img.alt = 'Foto de michi en favoritos'
+            img.width = "250"
+            const button = document.createElement('button')
+            button.innerText = 'Quitar de favoritos'
+            article.append(img)
+            article.append(button)
+            divFavoriteMichis.append(article)
+            console.log(img)
+        }
+    }) 
+    
 }
-getFavoriteMichis()
+
+async function saveFavoriteMichis() {
+    const res = await fetch(API_FAVORITES, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            image_id: '75k',
+        })
+    })
+
+    console.log(res)
+}
 // ============================
 
-// el maestro lo hizo asi
-async function reload() {
-    const res = await fetch(API)
-    const data = await res.json()
+button.addEventListener('click', getResults);
+getResults();
+getFavoriteMichis();
 
-    img1.src = data[0].url
-}
-// ============================
-
-button.addEventListener('click', getResults)
-getResults()
- 
